0d0501a1e93818daa976b43170a73bf3c4413294
DLllwj <895844661@qq.com>
2021-02-17 12:19:53 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-Alphabet></city-Alphabet>
+    <city-list :hot="hotCities" :cities="cities"></city-list>
+    <city-Alphabet :cities="cities"></city-Alphabet>
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -19,6 +20,28 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet,
+  },
+  data() {
+    return {
+      hotCities: [],
+      cities: {}
+    }
+  },
+  methods: {
+    getCityInfo() {
+      axios.get('/static/mock/city.json').then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc(res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted() {
+    this.getCityInfo()
   }
 }
 </script>
