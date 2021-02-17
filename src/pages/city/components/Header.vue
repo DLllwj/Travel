9a5bc3ba92cdd051d7173135e8ed58aacdb8a8ac
DLllwj <895844661@qq.com>
2021-02-17 09:01:17 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <router-link to="/" tag="div">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+    城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header {
+  overflow: hidden;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  text-align: center;
+  color: #fff;
+  background: #00bcd4;
+  font-size: 0.32rem;
+  position: relative;
+
+  .header-back {
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,9 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}<span class="iconfont arrow-icon">&#xe62f;</span>
-    </div>
+    <router-link to='/city' tag="div">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe62f;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
