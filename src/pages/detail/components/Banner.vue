d90a84465ed070c486224881ee492e00477d7208
DLllwj <895844661@qq.com>
2021-02-20 16:55:30 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg">
+      <div class="banner-info">
+        <div class="banner-title">故宫(AAAAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe7be;</span>12</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGrllary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data() {
+    return {
+      showGrllary: false,
+      imgs: ["//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg", "https://imgs.qunarzz.com/sight/p0/1909/4e/4e1afc2e1133ade7a3.img.jpg_350x240_2646c984.jpg"]
+    }
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGrllary = true
+    },
+    handleGallaryClose() {
+      this.showGrllary = false
+    }
+  },
+  components: {
+    CommonGallary,
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner {
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+
+  .banner-img {
+    width: 100%;
+  }
+
+  .banner-info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: 0.6rem;
+    color: #fff;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+
+    .banner-title {
+      flex: 1;
+      font-size: 0.32rem;
+      padding: 0 0.2rem;
+    }
+
+    .banner-number {
+      height: 0.32rem;
+      margin-top: 0.14rem;
+      padding: 0 0.4rem;
+      line-height: 0.32rem;
+      border-radius: 0.2rem;
+      background: rgba(0, 0, 0, 0.8);
+      font-size: 0.24rem;
+
+      .banner-icon {
+        font-size: 0.24rem;
+      }
+    }
+  }
+}
+</style>
